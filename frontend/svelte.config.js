06dc69fd2cb706900dc3371ec68814cd502c6285
Evgeny Klinnikov <mostransformer@gmail.com>
2021-11-14 11:00:40 +0300

    front: initial commit

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +12,13 @@ const config = {
 	],
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter({ out: "build" }),
+		target: "#svelte",
+		router: true,
+		ssr: true,
+		hydrate: true,
+		trailingSlash: "never",
+		amp: false
 	}
 };
 
