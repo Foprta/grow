df3ed8e6b9da86465a21e4f9b88ef1d7397c4401
klalvit <klalvit1@gmail.com>
2021-12-09 20:03:08 +0300

    back: старт коинмаркеткапа

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:12345@localhost/grow'
 db = SQLAlchemy(app)
+cors = CORS(app)
 
 from app import routes, models
 
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,19 @@
 from app import app, db
 from app.models import Users
+from app.coinmarketcap import get_coins
 
 @app.route('/')
 def index():
+    return "HELLO MAN"
+
+@app.route('/api/user', methods=["POST"])
+def user_create():
     user = Users()
     db.session.add(user)
     db.session.commit()
     return user.id
+
+@app.route('/api/coin')
+def coin_create():
+    get_coins()
+    return "sd"
